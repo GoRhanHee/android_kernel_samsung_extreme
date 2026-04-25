6d465f5592ea764187a4774b2ae8d2c1e7e616de
GoRhanHee <gorhanhee01@gmail.com>
2026-04-25 20:32:33 +0900

    build: fix import KSU code
    Signed-off-by: GoRhanHee <gorhanhee01@gmail.com>

--- build.sh
+++ build.sh
@@ -44,7 +44,7 @@ done
 echo "Preparing the build environment..."
 
 # Import KernelSU
-curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -s legacy
+curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -
 
 pushd $(dirname "$0") > /dev/null
 CORES=`cat /proc/cpuinfo | grep -c processor`
